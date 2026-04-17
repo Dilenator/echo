52f23e1603f6be429d8e2c5de56b04e0ffeddb4b
Dilen Shingadia <dilen_shingadia15@hotmail.com>
2026-04-17 19:04:49 +0100

    added to add_entry methods for image selection date selectionand saving entry to Isar.Untested- the scaffold build is still empty, so work needed to build widget page using methods

--- lib/pages/add_entry.dart
+++ lib/pages/add_entry.dart
@@ -36,19 +36,84 @@ class _AddEntryPageState extends State<AddEntryPage>{
   Future<void> _pickImage() async{
     final picker = ImagePicker();
     final pickedImage = await picker.pickImage(source: ImageSource.gallery);
-    if(pickedImage != null){
-      setState(() {
-        _imagePath = pickedImage.path;
-      });
+    if(pickedImage != null) {
+      setState(() => _imagePath = pickedImage.path);
     }
   }
+
+  Future<void> _pickDate() async {
+    final pickedDate = await showDatePicker(
+      context: context, 
+      initialDate: _selectedDate, //set default to current date  
+      firstDate: DateTime(2000), // earliest date is 2000.
+      lastDate: _selectedDate //t he max is set to the current date, so you cant make future entries
+    );
+    if (pickedDate != null) {
+      setState(() => _selectedDate = pickedDate);
+    }
+  }
+
+  Future<void> _saveEntry() async {
+    final newEntry = Entry();
+      newEntry.title = _whatHappened.text;
+      newEntry.content = _whyItHappened.text;
+      newEntry.mood = _selectedMood;
+      newEntry.date = _selectedDate;
+      newEntry.imagePath = _imagePath;
+
+    await context.read<EntryDB>().addEntry(newEntry);
+
+    if (!mounted) return;
+    Navigator.pop(context);
+
+  }
+
+  String _formatDate(DateTime date) {
+    return '${date.day}/${date.month}/${date.year}';
+  }
+
+  @override
+  void dispose() {
+    _whatHappened.dispose();
+    _whyItHappened.dispose();
+    super.dispose();
+    }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: _cream,
-      body: const Center(child: Text('Add Entry'),
-      ),
-    );
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(24),
+            child: Container(
+              decoration: BoxDecoration(
+                color: _blue,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              padding: const EdgeInsets.all(24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Add New Entry',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                ]
+              )
+            ),
 
+          ),
+        ),
+      ),
+    ); 
   }
+
+
 }
